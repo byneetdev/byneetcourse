d16ff3e979a94392eeadba57dd584d21b73ee132
byneet <khairul.id28@gmail.com>
2020-02-08 22:15:50 +0700

    add login service

---
+++ lib/src/modules/login/login_service.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
+
+class LoginService with ChangeNotifier {
+  FirebaseAuth _auth;
+  FirebaseUser _user;
+  GoogleSignIn _googleSignIn;
+  Status _status = Status.Uninitialized;
+
+  LoginService.instance()
+      : _auth = FirebaseAuth.instance,
+        _googleSignIn = GoogleSignIn() {
+    _auth.onAuthStateChanged.listen(_onAuthStateChanged);
+  }
+
+  Status get status => _status;
+  FirebaseUser get user => _user;
+
+  Future<bool> signIn(String email, String password) async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future<bool> signInWithGoogle() async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+
+      return true;
+    } catch (e) {
+      print(e);
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future signOut() async {
+    _auth.signOut();
+    _googleSignIn.signOut();
+    _status = Status.Unauthenticated;
+    notifyListeners();
+    return Future.delayed(Duration.zero);
+  }
+
+  Future<void> _onAuthStateChanged(FirebaseUser firebaseUser) async {
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = firebaseUser;
+
+      //tiap login/buka app,getdata detail user, lalu disimpan di Accountservice:AccountDetail; dak jadi ak boros mana wkwkw
+      // AccountService().getDocumentById(firebaseUser.uid);
+
+      _status = Status.Authenticated;
+    }
+    notifyListeners();
+  }
+}
