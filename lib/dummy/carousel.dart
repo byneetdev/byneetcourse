32a8d5d12b18905549ccf006e62f2fd56c77df44
byneet <khairul.id28@gmail.com>
2020-02-15 06:45:59 +0700

    commented

--- lib/dummy/carousel.dart
+++ lib/dummy/carousel.dart
@@ -1,29 +1,29 @@
-class Carousel {
-  final String name;
-  final String status;
-  final String urlimage;
-  Carousel({this.name, this.status, this.urlimage});
-}
+// class Carousel {
+//   final String name;
+//   final String status;
+//   final String urlimage;
+//   Carousel({this.name, this.status, this.urlimage});
+// }
 
-List<Carousel> dummyCarousel = [
-  Carousel(
-    name: "Machine Learning",
-    status: "Premium",
-    urlimage: "images/byneet3.png",
-  ),
-  Carousel(
-    name: "Full-Stack Web Designer",
-    status: "Gratis",
-    urlimage: "images/byneet3.png",
-  ),
-  Carousel(
-    name: "Android Developer",
-    status: "Gratis",
-    urlimage: "images/byneet3.png",
-  ),
-  Carousel(
-    name: "Artificial Intelligent",
-    status: "Premium",
-    urlimage: "images/byneet3.png",
-  ),
-];
+// List<Carousel> dummyCarousel = [
+//   Carousel(
+//     name: "Machine Learning",
+//     status: "Premium",
+//     urlimage: "images/byneet3.png",
+//   ),
+//   Carousel(
+//     name: "Full-Stack Web Designer",
+//     status: "Gratis",
+//     urlimage: "images/byneet3.png",
+//   ),
+//   Carousel(
+//     name: "Android Developer",
+//     status: "Gratis",
+//     urlimage: "images/byneet3.png",
+//   ),
+//   Carousel(
+//     name: "Artificial Intelligent",
+//     status: "Premium",
+//     urlimage: "images/byneet3.png",
+//   ),
+// ];
--- lib/dummy/kelas.dart
+++ lib/dummy/kelas.dart
@@ -1,292 +1,292 @@
-class Kelas {
-  final String name;
-  final String urlimage;
-  final String status;
-  final String description;
-  final List<Screenshot> screenshot;
-  final List<Theory> theory;
+// class Kelas {
+//   final String name;
+//   final String urlimage;
+//   final String status;
+//   final String description;
+//   final List<Screenshot> screenshot;
+//   final List<Theory> theory;
 
-  Kelas({
-    this.name,
-    this.urlimage,
-    this.status,
-    this.description,
-    this.screenshot,
-    this.theory,
-  });
-}
-
-class Screenshot {
-  final String urlimage;
-  Screenshot({this.urlimage});
-}
-
-class Theory {
-  final String title;
-  final List<Theory> exam;
-  Theory({this.title, this.exam});
-}
-
-// class Exam {
-//   final String title;
-//   Theory({this.title});
+//   Kelas({
+//     this.name,
+//     this.urlimage,
+//     this.status,
+//     this.description,
+//     this.screenshot,
+//     this.theory,
+//   });
 // }
 
-List<Kelas> dummyKelas = [
-  Kelas(
-    name: "Full-Stack Android Developer",
-    urlimage: "images/gambar1.jpg",
-    status: "Gratis",
-    description:
-        "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
-    screenshot: [
-      Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
-      Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
-      Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
-      Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
-    ],
-    theory: [
-      Theory(
-        title: "Warming Up",
-        exam: [
-          Theory(title: "Ketentuan Belajar"),
-          Theory(title: "Kolaborasi Team"),
-        ],
-      ),
-      Theory(
-        title: "Warming Up II",
-        exam: [
-          Theory(title: "Perkenalan Materi"),
-          Theory(title: "Design First App"),
-        ],
-      ),
-      Theory(
-        title: "Explore Workspace",
-        exam: [
-          Theory(title: "Build First App"),
-          Theory(title: "Layout Android"),
-        ],
-      ),
-      Theory(
-        title: "Basic Android",
-        exam: [
-          Theory(title: "Warna Dinamis"),
-          Theory(title: "String Dinamis"),
-          Theory(title: "Components"),
-          Theory(title: "EditText & Drawable"),
-          Theory(title: "Java Pemula"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer",
-        exam: [
-          Theory(title: "Design Asset"),
-          Theory(title: "Install Fonts"),
-          Theory(title: "XML Splash Screen"),
-          Theory(title: "XML Get Started"),
-          Theory(title: "XML Sign In"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer II",
-        exam: [
-          Theory(title: "XML Register I"),
-          Theory(title: "XML Register II"),
-          Theory(title: "XML Success Register"),
-        ],
-      ),
-    ],
-  ),
-  Kelas(
-    name: "Full-Stack Web Developer",
-    urlimage: "images/gambar1.jpg",
-    status: "Premium",
-    description:
-        "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
-    screenshot: [
-      Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
-      Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
-      Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
-      Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
-    ],
-    theory: [
-      Theory(
-        title: "Warming Up",
-        exam: [
-          Theory(title: "Ketentuan Belajar"),
-          Theory(title: "Kolaborasi Team"),
-        ],
-      ),
-      Theory(
-        title: "Warming Up II",
-        exam: [
-          Theory(title: "Perkenalan Materi"),
-          Theory(title: "Design First App"),
-        ],
-      ),
-      Theory(
-        title: "Explore Workspace",
-        exam: [
-          Theory(title: "Build First App"),
-          Theory(title: "Layout Android"),
-        ],
-      ),
-      Theory(
-        title: "Basic Android",
-        exam: [
-          Theory(title: "Warna Dinamis"),
-          Theory(title: "String Dinamis"),
-          Theory(title: "Components"),
-          Theory(title: "EditText & Drawable"),
-          Theory(title: "Java Pemula"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer",
-        exam: [
-          Theory(title: "Design Asset"),
-          Theory(title: "Install Fonts"),
-          Theory(title: "XML Splash Screen"),
-          Theory(title: "XML Get Started"),
-          Theory(title: "XML Sign In"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer II",
-        exam: [
-          Theory(title: "XML Register I"),
-          Theory(title: "XML Register II"),
-          Theory(title: "XML Success Register"),
-        ],
-      ),
-    ],
-  ),
-  Kelas(
-    name: "Swift Developer",
-    urlimage: "images/gambar1.jpg",
-    status: "Gratis",
-    description:
-        "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
-    screenshot: [
-      Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
-      Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
-      Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
-      Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
-    ],
-    theory: [
-      Theory(
-        title: "Warming Up",
-        exam: [
-          Theory(title: "Ketentuan Belajar"),
-          Theory(title: "Kolaborasi Team"),
-        ],
-      ),
-      Theory(
-        title: "Warming Up II",
-        exam: [
-          Theory(title: "Perkenalan Materi"),
-          Theory(title: "Design First App"),
-        ],
-      ),
-      Theory(
-        title: "Explore Workspace",
-        exam: [
-          Theory(title: "Build First App"),
-          Theory(title: "Layout Android"),
-        ],
-      ),
-      Theory(
-        title: "Basic Android",
-        exam: [
-          Theory(title: "Warna Dinamis"),
-          Theory(title: "String Dinamis"),
-          Theory(title: "Components"),
-          Theory(title: "EditText & Drawable"),
-          Theory(title: "Java Pemula"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer",
-        exam: [
-          Theory(title: "Design Asset"),
-          Theory(title: "Install Fonts"),
-          Theory(title: "XML Splash Screen"),
-          Theory(title: "XML Get Started"),
-          Theory(title: "XML Sign In"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer II",
-        exam: [
-          Theory(title: "XML Register I"),
-          Theory(title: "XML Register II"),
-          Theory(title: "XML Success Register"),
-        ],
-      ),
-    ],
-  ),
-  Kelas(
-    name: "Artificial Intelligent with Phyton",
-    urlimage: "images/gambar1.jpg",
-    status: "Premium",
-    description:
-        "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
-    screenshot: [
-      Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
-      Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
-      Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
-      Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
-    ],
-    theory: [
-      Theory(
-        title: "Warming Up",
-        exam: [
-          Theory(title: "Ketentuan Belajar"),
-          Theory(title: "Kolaborasi Team"),
-        ],
-      ),
-      Theory(
-        title: "Warming Up II",
-        exam: [
-          Theory(title: "Perkenalan Materi"),
-          Theory(title: "Design First App"),
-        ],
-      ),
-      Theory(
-        title: "Explore Workspace",
-        exam: [
-          Theory(title: "Build First App"),
-          Theory(title: "Layout Android"),
-        ],
-      ),
-      Theory(
-        title: "Basic Android",
-        exam: [
-          Theory(title: "Warna Dinamis"),
-          Theory(title: "String Dinamis"),
-          Theory(title: "Components"),
-          Theory(title: "EditText & Drawable"),
-          Theory(title: "Java Pemula"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer",
-        exam: [
-          Theory(title: "Design Asset"),
-          Theory(title: "Install Fonts"),
-          Theory(title: "XML Splash Screen"),
-          Theory(title: "XML Get Started"),
-          Theory(title: "XML Sign In"),
-        ],
-      ),
-      Theory(
-        title: "UI Developer II",
-        exam: [
-          Theory(title: "XML Register I"),
-          Theory(title: "XML Register II"),
-          Theory(title: "XML Success Register"),
-        ],
-      ),
-    ],
-  ),
-];
+// class Screenshot {
+//   final String urlimage;
+//   Screenshot({this.urlimage});
+// }
+
+// class Theory {
+//   final String title;
+//   final List<Theory> exam;
+//   Theory({this.title, this.exam});
+// }
+
+// // class Exam {
+// //   final String title;
+// //   Theory({this.title});
+// // }
+
+// List<Kelas> dummyKelas = [
+//   Kelas(
+//     name: "Full-Stack Android Developer",
+//     urlimage: "images/gambar1.jpg",
+//     status: "Gratis",
+//     description:
+//         "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
+//     screenshot: [
+//       Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
+//       Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
+//       Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
+//       Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
+//     ],
+//     theory: [
+//       Theory(
+//         title: "Warming Up",
+//         exam: [
+//           Theory(title: "Ketentuan Belajar"),
+//           Theory(title: "Kolaborasi Team"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Warming Up II",
+//         exam: [
+//           Theory(title: "Perkenalan Materi"),
+//           Theory(title: "Design First App"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Explore Workspace",
+//         exam: [
+//           Theory(title: "Build First App"),
+//           Theory(title: "Layout Android"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Basic Android",
+//         exam: [
+//           Theory(title: "Warna Dinamis"),
+//           Theory(title: "String Dinamis"),
+//           Theory(title: "Components"),
+//           Theory(title: "EditText & Drawable"),
+//           Theory(title: "Java Pemula"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer",
+//         exam: [
+//           Theory(title: "Design Asset"),
+//           Theory(title: "Install Fonts"),
+//           Theory(title: "XML Splash Screen"),
+//           Theory(title: "XML Get Started"),
+//           Theory(title: "XML Sign In"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer II",
+//         exam: [
+//           Theory(title: "XML Register I"),
+//           Theory(title: "XML Register II"),
+//           Theory(title: "XML Success Register"),
+//         ],
+//       ),
+//     ],
+//   ),
+//   Kelas(
+//     name: "Full-Stack Web Developer",
+//     urlimage: "images/gambar1.jpg",
+//     status: "Premium",
+//     description:
+//         "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
+//     screenshot: [
+//       Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
+//       Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
+//       Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
+//       Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
+//     ],
+//     theory: [
+//       Theory(
+//         title: "Warming Up",
+//         exam: [
+//           Theory(title: "Ketentuan Belajar"),
+//           Theory(title: "Kolaborasi Team"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Warming Up II",
+//         exam: [
+//           Theory(title: "Perkenalan Materi"),
+//           Theory(title: "Design First App"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Explore Workspace",
+//         exam: [
+//           Theory(title: "Build First App"),
+//           Theory(title: "Layout Android"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Basic Android",
+//         exam: [
+//           Theory(title: "Warna Dinamis"),
+//           Theory(title: "String Dinamis"),
+//           Theory(title: "Components"),
+//           Theory(title: "EditText & Drawable"),
+//           Theory(title: "Java Pemula"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer",
+//         exam: [
+//           Theory(title: "Design Asset"),
+//           Theory(title: "Install Fonts"),
+//           Theory(title: "XML Splash Screen"),
+//           Theory(title: "XML Get Started"),
+//           Theory(title: "XML Sign In"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer II",
+//         exam: [
+//           Theory(title: "XML Register I"),
+//           Theory(title: "XML Register II"),
+//           Theory(title: "XML Success Register"),
+//         ],
+//       ),
+//     ],
+//   ),
+//   Kelas(
+//     name: "Swift Developer",
+//     urlimage: "images/gambar1.jpg",
+//     status: "Gratis",
+//     description:
+//         "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
+//     screenshot: [
+//       Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
+//       Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
+//       Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
+//       Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
+//     ],
+//     theory: [
+//       Theory(
+//         title: "Warming Up",
+//         exam: [
+//           Theory(title: "Ketentuan Belajar"),
+//           Theory(title: "Kolaborasi Team"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Warming Up II",
+//         exam: [
+//           Theory(title: "Perkenalan Materi"),
+//           Theory(title: "Design First App"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Explore Workspace",
+//         exam: [
+//           Theory(title: "Build First App"),
+//           Theory(title: "Layout Android"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Basic Android",
+//         exam: [
+//           Theory(title: "Warna Dinamis"),
+//           Theory(title: "String Dinamis"),
+//           Theory(title: "Components"),
+//           Theory(title: "EditText & Drawable"),
+//           Theory(title: "Java Pemula"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer",
+//         exam: [
+//           Theory(title: "Design Asset"),
+//           Theory(title: "Install Fonts"),
+//           Theory(title: "XML Splash Screen"),
+//           Theory(title: "XML Get Started"),
+//           Theory(title: "XML Sign In"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer II",
+//         exam: [
+//           Theory(title: "XML Register I"),
+//           Theory(title: "XML Register II"),
+//           Theory(title: "XML Success Register"),
+//         ],
+//       ),
+//     ],
+//   ),
+//   Kelas(
+//     name: "Artificial Intelligent with Phyton",
+//     urlimage: "images/gambar1.jpg",
+//     status: "Premium",
+//     description:
+//         "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae",
+//     screenshot: [
+//       Screenshot(urlimage: "images/6c81844156f8a914912639a3037a0938.png"),
+//       Screenshot(urlimage: "images/32a31152c0395e92032e0b0bdf786a6a.png"),
+//       Screenshot(urlimage: "images/66e3a4b383d5050ddc48fe921da44f30.png"),
+//       Screenshot(urlimage: "images/893e1c7b84297556550f6c64d4944a87.jpg"),
+//     ],
+//     theory: [
+//       Theory(
+//         title: "Warming Up",
+//         exam: [
+//           Theory(title: "Ketentuan Belajar"),
+//           Theory(title: "Kolaborasi Team"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Warming Up II",
+//         exam: [
+//           Theory(title: "Perkenalan Materi"),
+//           Theory(title: "Design First App"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Explore Workspace",
+//         exam: [
+//           Theory(title: "Build First App"),
+//           Theory(title: "Layout Android"),
+//         ],
+//       ),
+//       Theory(
+//         title: "Basic Android",
+//         exam: [
+//           Theory(title: "Warna Dinamis"),
+//           Theory(title: "String Dinamis"),
+//           Theory(title: "Components"),
+//           Theory(title: "EditText & Drawable"),
+//           Theory(title: "Java Pemula"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer",
+//         exam: [
+//           Theory(title: "Design Asset"),
+//           Theory(title: "Install Fonts"),
+//           Theory(title: "XML Splash Screen"),
+//           Theory(title: "XML Get Started"),
+//           Theory(title: "XML Sign In"),
+//         ],
+//       ),
+//       Theory(
+//         title: "UI Developer II",
+//         exam: [
+//           Theory(title: "XML Register I"),
+//           Theory(title: "XML Register II"),
+//           Theory(title: "XML Success Register"),
+//         ],
+//       ),
+//     ],
+//   ),
+// ];
