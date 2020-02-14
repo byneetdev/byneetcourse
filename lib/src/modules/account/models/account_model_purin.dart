bca15b986acaa3f384996b3f956590e9c5925230
byneet <khairul.id28@gmail.com>
2020-02-15 06:47:40 +0700

    accoun model - need improve

--- lib/src/modules/account/models/account_model_purin.dart
+++ lib/src/modules/account/models/account_model_purin.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 //nanti tinggal atur model nye jak..
 class AccountModel {
-  final String data;
-  AccountModel({this.data});
+  final String id;
+  final String urlImg;
+  final String nama;
+  AccountModel({this.urlImg, this.id, this.nama});
 
-  factory AccountModel.fromFirestore(DocumentSnapshot doc) =>
-      AccountModel(data: doc.toString());
+  factory AccountModel.fromFirestore(DocumentSnapshot doc) => AccountModel(
+      id: doc.documentID, urlImg: doc.data["urlImg"], nama: doc.data["nama"]);
 
   Map<String, dynamic> toMap() {
     return {};
