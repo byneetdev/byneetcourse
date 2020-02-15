eedfacd5bf765ee659ca5cd29a7e8212b6969fff
byneet <khairul.id28@gmail.com>
2020-02-15 13:15:15 +0700

    create nodata_container

---
+++ lib/src/modules/nodata_container.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoDataContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('No data'),
+      ),
+    );
+  }
+}
