d664921efc01179c07146dff9a1f810d7e5c0493
byneet <khairul.id28@gmail.com>
2020-02-23 11:50:08 +0700

    auth loading langsung home + load data wishlist & mycourse

--- lib/src/modules/splashScreen/screens/android/splashScreen_android.dart
+++ lib/src/modules/splashScreen/screens/android/splashScreen_android.dart
@@ -1,6 +1,8 @@
-import 'dart:async';
+// import 'dart:async';
+import 'package:byneetcourseapp/src/modules/account/mycourse_repository.dart';
 import 'package:byneetcourseapp/src/modules/login/services/login_service.dart';
 import 'package:byneetcourseapp/src/modules/login/screens/android/login_android.dart';
+import 'package:byneetcourseapp/src/modules/wishlist/wishlist_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,33 +16,44 @@ class SplashScreen extends StatefulWidget {
 /// Component UI
 class _SplashScreenState extends State<SplashScreen> {
   /// Setting duration in splash screen
-  startTime() async {
-    return new Timer(Duration(milliseconds: 4500), _navigatorPage);
-  }
+  // startTime() async {
+  //   return new Timer(Duration(milliseconds: 4500), _navigatorPage);
+  // }
 
   /// To navigate layout change
-  void _navigatorPage() {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) =>
-              Consumer<LoginService>(builder: (context, user, _) {
-            if (user.status != Status.Authenticated) return LoginAndroid();
-            return BottomNavBar();
-          }),
-        ));
-  }
+  // void _navigatorPage() {
+  //   Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (BuildContext context) =>
+  //             Consumer<LoginService>(builder: (context, user, _) {
+  //           if (user.status == Status.Unauthenticated) return LoginAndroid();
+  //           return BottomNavBar();
+  //         }),
+  //       ));
+  // }
 
   /// Declare startTime to InitState
   @override
   void initState() {
     super.initState();
-    startTime();
+    // startTime();
   }
 
   /// Code Create UI Splash Screen
   @override
   Widget build(BuildContext context) {
+    //biar dak muncul screen login agik, jadi mun dah authenticated langsung home,
+    final user = Provider.of<LoginService>(context);
+    if (user.status == Status.Authenticated) {
+      Provider.of<WishListRepository>(context, listen: false)
+          .refreshRepository(user.user.uid);
+      Provider.of<MyCourseRepository>(context, listen: false)
+          .getData(user.user.uid);
+      return BottomNavBar();
+    }
+    if (user.status == Status.Unauthenticated) return LoginAndroid();
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
