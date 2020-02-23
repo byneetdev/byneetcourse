f4eb0148dc5599755fc6fb201957e43f7915b6f1
byneet <khairul.id28@gmail.com>
2020-02-23 11:50:28 +0700

    wishlist fix-fix-an

--- lib/src/modules/wishlist/screens/android/wishlist_android.dart
+++ lib/src/modules/wishlist/screens/android/wishlist_android.dart
@@ -1,25 +1,26 @@
 import 'package:byneetcourseapp/src/modules/course/models/course_model_purin.dart';
+import 'package:byneetcourseapp/src/modules/loading_container.dart';
 import 'package:byneetcourseapp/src/modules/login/services/login_service.dart';
 import 'package:byneetcourseapp/src/modules/nodata_container.dart';
 import 'package:byneetcourseapp/src/modules/wishlist/widgets/wishlist_widget.dart';
-import 'package:byneetcourseapp/src/modules/wishlist/wishlist_service.dart';
+import 'package:byneetcourseapp/src/modules/wishlist/wishlist_repository.dart';
 import 'package:byneetcourseapp/src/tools/constColor.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../../../loading_container.dart';
 
 class WishlistAndroid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<LoginService>(context);
+    final wishlist = Provider.of<WishListRepository>(context);
+    print(wishlist.wishlist);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: CustomColor.background,
         elevation: 0,
       ),
       body: FutureBuilder<List<CourseModel>>(
-          future: WishListService(user.user.uid).getDataCollection(),
+          future: wishlist.getState(user.user.uid),
           builder: (context, snapshot) {
             if (snapshot.connectionState != ConnectionState.done)
               return LoadingContainer();
--- lib/src/modules/wishlist/wishlist_service.dart
+++ lib/src/modules/wishlist/wishlist_service.dart
@@ -1,28 +1,29 @@
 import 'package:byneetcourseapp/src/utils/api.dart';
 import 'package:byneetcourseapp/src/modules/course/models/course_model_purin.dart';
-import 'package:flutter/material.dart';
 
-class WishListService with ChangeNotifier {
+class WishListService {
   final String idUser;
   Api _api;
   WishListService(this.idUser) {
     _api = Api('users/$idUser/wishlist');
   }
 
-  //tempat simpan data
-  List<CourseModel> wishlist;
-
   Future<List<CourseModel>> getDataCollection() async {
     var res = await _api.getDataCollection();
     if (res.documents.length > 0) {
-      wishlist =
+      List<CourseModel> wishlist =
           res.documents.map((doc) => CourseModel.fromFirestore(doc)).toList();
+      print('get wishlist collection' + wishlist.length.toString());
       return wishlist;
     }
     return null;
   }
 
-  Future<void> setWishlist(CourseModel data) async {
-    await _api.setDocument(data.uid, data.toMap());
+  Future<void> setWishlist(CourseModel data, bool isInList) async {
+    if (isInList) {
+      await _api.removeDocument(data.uid);
+    } else {
+      await _api.setDocument(data.uid, data.toMap());
+    }
   }
 }
