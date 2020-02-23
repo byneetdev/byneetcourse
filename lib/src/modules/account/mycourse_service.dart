9d4daeee32b166579c29740b6a0ee3ba530ff2c7
byneet <khairul.id28@gmail.com>
2020-02-23 11:43:13 +0700

    my course repository + service

---
+++ lib/src/modules/account/mycourse_service.dart
@@ -0,0 +1,40 @@
+import 'package:byneetcourseapp/src/utils/api.dart';
+import 'package:byneetcourseapp/src/modules/course/models/course_model_purin.dart';
+import 'package:flutter/cupertino.dart';
+
+class MycourseService with ChangeNotifier {
+  final String idUser;
+  Api _api;
+  MycourseService(this.idUser) {
+    _api = Api('users/$idUser/mycourses');
+  }
+
+  List<CourseModel> listMyCourse;
+  CourseModel myCourse;
+
+  Future<List<CourseModel>> getCollection() async {
+    var res = await _api.getDataCollection();
+    if (res.documents.length > 0) {
+      listMyCourse =
+          res.documents.map((doc) => CourseModel.fromFirestore(doc)).toList();
+      return listMyCourse;
+    }
+    return null;
+  }
+
+  Future<CourseModel> getDocument(String idCourse) async {
+    var doc = await _api.getDocumentById(idCourse);
+    myCourse = CourseModel.fromFirestore(doc);
+    return myCourse;
+  }
+
+  //ngeset document di collection user/$iduser/mysourse, triger pas user daftar course
+  Future<void> setDocument(CourseModel data) async {
+    await _api.setDocument(data.uid, data.toMap());
+  }
+
+  //nyimpan progress berdasarkan idmateri,..
+  Future<void> updateProgressDone(String idmycourse, List<String> data) async {
+    await _api.updateDocument(idmycourse, {"progress": data});
+  }
+}
